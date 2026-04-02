61c917a8dd27732fcfce12af22016a8ff4098427
nm254744 <nicolai.mathar@ima.rwth-aachen.de>
2026-04-02 10:25:43 +0200

    remove menu and add settings button to app bar

--- app.vue
+++ app.vue
@@ -6,28 +6,12 @@
           <v-icon class="mr-2">mdi-car-electric</v-icon>
           EV Ladekosten Rechner
         </v-app-bar-title>
-      </v-app-bar>
 
-      <v-navigation-drawer permanent>
-        <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-view-dashboard"
-            title="Übersicht"
-            to="/"
-            exact
-          />
-          <v-list-item
-            prepend-icon="mdi-lightning-bolt"
-            title="Neuer Ladevorgang"
-            to="/charging"
-          />
-          <v-list-item
-            prepend-icon="mdi-cog"
-            title="Einstellungen"
-            to="/settings"
-          />
-        </v-list>
-      </v-navigation-drawer>
+        <v-spacer></v-spacer>
+        <v-btn icon to="/settings" title="Einstellungen" :hover="'Einstellungen'">
+          <v-icon>mdi-cog</v-icon>
+        </v-btn>
+      </v-app-bar>
     </ClientOnly>
 
     <v-main>
